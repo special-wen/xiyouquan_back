eaf67526febd6ea18c3c66b1669aefccc387c6cb
City-Zero <liyutong19961231@gmail.com>
2019-05-15 21:20:36 +0800

    feat:数据为0

--- routes/topic.js
+++ routes/topic.js
@@ -19,37 +19,50 @@ router.get('/topic', function(req, res, next) {
       counts = 0;
     } else {
       counts = 10;
-    }    
-    topic_cards.map(item => {
-      //更改图片格式
-      if (item.pics !== null) {
-        let pics = item.pics.split(",");
-        item.pics = pics;      
-      } else {
-        item.pics = item.pics;
-      }
-      //查找 topic_id对应的like和comment的总数
-      let commentCount_sql = 'select count(1) from comment where topic_id=?';
-      db.query(commentCount_sql,item.topic_id,(err,count) => {
-        item.comment_count = count[0]["count(1)"];
-        let likeCount_sql = 'select count(1) from `like` where topic_id=?';
-        db.query(likeCount_sql,item.topic_id,(err,count) => {
-          item.like_count = count[0]["count(1)"];
-          //获取当前用户是否点赞了
-          //先判断session
-          if (req.session.userInfo) {
-            let cur_id = req.session.userInfo.uid;
-            let likeCur_sql = 'select * from `like` where uid =? and topic_id =?'
-            db.query(likeCur_sql,[cur_id,item.topic_id],(err,flag) => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              if (flag.length === 0) {
-                item.like = false;
-              } else {
-                item.like = true;
-              }
+    }
+    console.log(topic_cards);
+    if (topic_cards.length > 0) {
+      topic_cards.map(item => {
+        //更改图片格式
+        if (item.pics !== null) {
+          let pics = item.pics.split(",");
+          item.pics = pics;      
+        } else {
+          item.pics = item.pics;
+        }
+        //查找 topic_id对应的like和comment的总数
+        let commentCount_sql = 'select count(1) from comment where topic_id=?';
+        db.query(commentCount_sql,item.topic_id,(err,count) => {
+          item.comment_count = count[0]["count(1)"];
+          let likeCount_sql = 'select count(1) from `like` where topic_id=?';
+          db.query(likeCount_sql,item.topic_id,(err,count) => {
+            item.like_count = count[0]["count(1)"];
+            //获取当前用户是否点赞了
+            //先判断session
+            if (req.session.userInfo) {
+              let cur_id = req.session.userInfo.uid;
+              let likeCur_sql = 'select * from `like` where uid =? and topic_id =?'
+              db.query(likeCur_sql,[cur_id,item.topic_id],(err,flag) => {
+                if (err) {
+                  console.log(err);
+                  return;
+                }
+                if (flag.length === 0) {
+                  item.like = false;
+                } else {
+                  item.like = true;
+                }
+                arr.push(item);
+                if (arr.length === topic_cards.length) {
+                  console.log(arr);
+                  res.json({
+                    "ok":1,
+                    "data":arr,
+                    "count":counts
+                  })
+                }
+              })
+            } else {
               arr.push(item);
               if (arr.length === topic_cards.length) {
                 console.log(arr);
@@ -59,21 +72,18 @@ router.get('/topic', function(req, res, next) {
                   "count":counts
                 })
               }
-            })
-          } else {
-            arr.push(item);
-            if (arr.length === topic_cards.length) {
-              console.log(arr);
-              res.json({
-                "ok":1,
-                "data":arr,
-                "count":counts
-              })
             }
-          }
+          })
         })
       })
-    })
+    }else {
+      res.json({
+        "ok":1,
+        "data":topic_cards,
+        "count":0
+      })
+    }  
+    
   })
 });
 
